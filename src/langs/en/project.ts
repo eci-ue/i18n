9186799452e22429b3b72dd712984db74db8e8a3
TheresiaT <37453744@qq.com>
2023-04-07 14:47:39 +0800

    fix:项目状态文案修改

--- src/langs/en/project.ts
+++ src/langs/en/project.ts
@@ -17,12 +17,12 @@ export default {
   },
   status: {
     pending: "Pending",
-    inProgress: "InProgress",  // 资源确认接受任务，正在完成
-    inInterrupt: "InInterrupt",   // PM 发出中断，任务处于中断中
+    inProgress: "In Progress",  // 资源确认接受任务，正在完成
+    inInterrupt: "In Interrupt",   // PM 发出中断，任务处于中断中
     submitted: "Submitted",   // 资源完成任务后提交，待PM审核
-    submittedInterrupt: "SubmittedInterrupt", // 任务中断后，资源提交，待PM审核
+    submittedInterrupt: "Submitted Interrupt", // 任务中断后，资源提交，待PM审核
     confirmed: "Confirmed",   // 资源完成任务后提交，PM确认
-    confirmedInterrupt: "ConfirmedInterrupt", // 任务中断后，资源提交，PM确认
+    confirmedInterrupt: "Confirmed Interrupt", // 任务中断后，资源提交，PM确认
   },
   detail: {
     mtFactor: "MT Factor",
