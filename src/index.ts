9b139e22ea21273f98ba2808090fcfeb27f26564
svonme <svon.me@gmail.com>
2023-08-21 10:44:58 +0800

    feat: i18n

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,9 @@
 import { I18n, LanguageType } from "@js-lion/i18n";
 import LanguageValues from "./langs/index";
 
-type Language = typeof LanguageValues[LanguageType.en];
+export type Language = typeof LanguageValues[LanguageType.en];
+
+export { LanguageType };
 
 export default function<T>(language?: string) {
   const i18n = I18n<T & Language>(language);
